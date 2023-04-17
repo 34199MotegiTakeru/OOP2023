9a5ea9138ccd1bae34c8bdb365cc5d207e1f85e4
茂木 豪琉 <OIBC34199@ojs.ac.jp>
2023-04-17 10:52:39 +0900

    不要部分削除

--- Sample/WindowsFormsApp1/Form1.cs
+++ Sample/WindowsFormsApp1/Form1.cs
@@ -19,7 +19,7 @@ namespace WindowsFormsApp1 {
         }
 
         private void btButton_Click(object sender, EventArgs e) {
-            //int ans = int.Parse(tbNum1.Text) + int.Parse(tbNum2.Text);
+            //int ans = int.Parse(tbNum1.Text) + int.Parse(tbNum2.Text)
             //tbAns.Text = ans.ToString();
 
             int num1 = int.Parse(tbNum1.Text);
