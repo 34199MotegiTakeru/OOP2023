977bbcfab0c4e19d69365cb6bca06e2ed613ae56
茂木 豪琉 <OIBC34199@ojs.ac.jp>
2023-05-08 10:54:07 +0900

    P２６まで

---
+++ Chapter01/Product/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductSample {
+    class Program {
+        static void Main(string[] args) {
+            //インスタンス生成
+            Product karinto = new Product(123, "かりんとう", 180);
+            Product daifuku = new Product(235, "大福もち", 160);
+
+            Console.WriteLine("かりんとうの税込み価格：" + karinto.GetPriceIncludingTax());
+            Console.WriteLine("大福もちの税込み価格：" + daifuku.GetPriceIncludingTax());
+        }
+    }
+}
