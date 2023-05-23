a5d0eeb53d920278d533f738648d5160088e329e
茂木 豪琉 <OIBC34199@ojs.ac.jp>
2023-05-23 11:12:06 +0900

    P90まで

--- Chapter03/Section03/Program.cs
+++ Chapter03/Section03/Program.cs
@@ -10,6 +10,15 @@ namespace Section03 {
             var list = new List<string> {
                "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong",
             };
+
+            //var exists = list.Exists(s => s[4] == 'n');
+            //Console.WriteLine(exists);
+            //var name = list.Find(s => s.Length == 9);
+            //Console.WriteLine(name);
+
+            var names = list.ConvertAll(s => s.ToLower());
+            names.ForEach(s => Console.WriteLine(s));
+            
         }
     }
 }
