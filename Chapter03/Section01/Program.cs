42a0fb284fd3c3e77518d958a9d518a2caf3cb59
茂木 豪琉 <OIBC34199@ojs.ac.jp>
2023-05-23 10:30:37 +0900

    P87 まで

--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -8,11 +8,13 @@ namespace Section01 {
     class Program {
 
         static void Main(string[] args) {
-            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
+            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4, 5, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-            int count = numbers.Count(n => n % 2 == 0);
-
-            Console.WriteLine(count);
+            //5の倍数
+            //int count = numbers.Count(n => n % 5 == 0 && n > 0);
+            //合計値
+            var sum = numbers.Where(n => n % 2 == 0).Sum();
+            Console.WriteLine(sum);
         }
     }
 }
