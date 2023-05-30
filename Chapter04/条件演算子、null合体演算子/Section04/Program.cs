a0b43c36bd0ef9f38de94cf5b4726a0d99225201
茂木 豪琉 <OIBC34199@ojs.ac.jp>
2023-05-30 10:01:33 +0900

    TryParseまで完成

--- Chapter04/条件演算子、null合体演算子/Section04/Program.cs
+++ Chapter04/条件演算子、null合体演算子/Section04/Program.cs
@@ -15,7 +15,41 @@ namespace Section04 {
             //Console.WriteLine(num);
             #endregion
 
+            #region null合体演算子
+            //string code = "12345";
+            //var message = GetMessage(code) ?? DefaultMessage();
+            //Console.WriteLine(message);
+            #endregion
 
+            #region null条件演算子
+            //Sale sale = new Sale() {
+            //    Amount = 1000,
+            //};
+            Sale sale = null;
+
+            //int」はnull許容型、「?.」はnull条件演算子
+            int? ret = sale?.Amount;
+            Console.WriteLine(ret);
+
+            #endregion
+        }
+
+        private static object GetMessage(object code) {
+            return null;
+        }
+
+        private static object DefaultMessage() {
+            return "Default Message";
         }
     }
+
+        //売り上げクラス
+        public class Sale {
+            //店舗名
+            public string ShopName { get; set; }
+            //商品カテゴリー
+            public string ProductCategory { get; set; }
+            //売上高
+            public int Amount { get; set; }
+        }
 }
