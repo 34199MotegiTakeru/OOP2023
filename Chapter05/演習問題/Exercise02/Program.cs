c39e5877c78e73e6c09bb6d9e1532cea9399517d
茂木 豪琉 <OIBC34199@ojs.ac.jp>
2023-05-31 12:08:14 +0900

    p158演習問題5.2完成

---
+++ Chapter05/演習問題/Exercise02/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("数字：");
+            var line = Console.ReadLine();
+            int num;
+            if (int.TryParse(line,out num)) {
+                Console.WriteLine("{0:#,#}", num);
+            }
+            else {
+                Console.WriteLine("変換できません");
+            }
+            
+
+        }
+    }
+}
