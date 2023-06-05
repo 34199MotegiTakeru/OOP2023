e6424a6e3dd21c6144289dddc71a4dd9cce59136
茂木 豪琉 <OIBC34199@ojs.ac.jp>
2023-06-05 10:52:11 +0900

    P158演習問題　5.3.4まで完成

--- Chapter03/Section03/Program.cs
+++ Chapter03/Section03/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Section03 {
+namespace Exercise03 {
     class Program {
         static void Main(string[] args) {
             var list = new List<string> {
--- Chapter05/演習問題/Section03/Program.cs
+++ Chapter05/演習問題/Section03/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Section03 {
+namespace Exercise03 {
     class Program {
         static void Main(string[] args) {
             var text = "Jackdaws love my big sphinx of quartz";
@@ -26,19 +26,26 @@ namespace Section03 {
         }
 
         private static void Exercise3_1(string text) {
-            
+            int target = text.Count(f => f == ' ');
+            Console.WriteLine(target);
         }
 
         private static void Exercise3_2(string text) {
-            
+            var replaced = text.Replace("big", "small");
+            Console.WriteLine(replaced);
         }
 
         private static void Exercise3_3(string text) {
-            
+            var tango = text.Split(' ').Count();
+            Console.WriteLine(tango);
         }
 
         private static void Exercise3_4(string text) {
-            
+            var ika = text.Split(' ').Where(n => n.Length <= 4);
+            foreach (var item in ika) {
+                Console.WriteLine(item);
+
+            }
         }
 
         private static void Exercise3_5(string text) {
