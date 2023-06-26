fb7fed8592f9f58cf8982e9866ded4114ae3bf73
茂木 豪琉 <OIBC34199@ojs.ac.jp>
2023-06-26 17:14:04 +0900

    設問２_提出

--- TestProject/Test02/Program.cs
+++ TestProject/Test02/Program.cs
@@ -36,32 +36,32 @@ namespace Test02 {
         //問題１　合計値を表示
         //　　　　出力結果【618】
         private static void Exercise01(List<int> numbers) {
-
-
-
+            var sum = numbers.Sum(s => s);
+            Console.Write(sum);
         }
 
         //問題２　偶数の最大値を表示
         //　　　　出力結果【94】
         private static void Exercise02(List<int> numbers) {
-
-
-
+            var max = numbers.Max(s => s);
+            Console.Write(max);
         }
         //問題３　昇順に並べて表示（遅延実行とする）
         //　　　　出力結果【12 14 17 20 31 35 40 48 53 76 87 91 94】
         private static void Exercise03(List<int> numbers) {
-
-
-
+            var syou = numbers.OrderBy(s => s);
+            foreach (var item in syou) {
+                Console.Write(item + " ");
+            }
         }
 
         //問題４　10以上50以下の数字のみを表示（即時実行でも可とする）
         //　　　　出力結果【12 14 20 40 35 31 17 48】
         private static void Exercise04(List<int> numbers) {
-
-
-
+            var nomi = numbers.Where(s => s >= 10 && 50 >= s);
+            foreach (var item in nomi) {
+                Console.Write(item + " ");
+            }
         }
     }
 }
