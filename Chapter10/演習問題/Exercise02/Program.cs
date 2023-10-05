6efd29d0ec5afa3f5181a4696394a737db0d7fdb
茂木豪琉 <OIBC34199@ojs.ac.jp>
2023-10-05 10:22:55 +0900

    Exercise02　完成

---
+++ Chapter10/演習問題/Exercise02/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+            Pickup3DigitNumber("sample.txt");
+        }
+
+        private static void Pickup3DigitNumber(string file) {
+            foreach (var line in File.ReadLines(file)) {
+                //var lines = Regex.Matches(line, @"[0-9]+{3,}");
+                var lines = Regex.Matches(line, @"\b[0-9]{3,}\b");
+
+                foreach (Match match in lines) {
+                    Console.WriteLine( match.Value);
+
+                }
+            }
+        }
+    }
+}
