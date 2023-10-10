01e2294703a9a7557fc86c24e93e64111f9e5866
茂木豪琉 <OIBC34199@ojs.ac.jp>
2023-10-10 11:52:39 +0900

    問題10.3　までできた

--- Chapter10/演習問題/Exercise06/Program.cs
+++ Chapter10/演習問題/Exercise06/Program.cs
@@ -8,9 +8,9 @@ using System.Threading.Tasks;
 namespace Exercise06 {
     class Program {
         static void Main(string[] args) {
-            var text = "しるし しんぶんし きたのたき きもの いろしろい トマト ヨクナクヨ";
+            var text = "しるし しんぶんし きたのたき きもの いろしろい トマト ヨクナクヨ ";
             
-            var pattern = @"\b(\w)\w\1\b"; //←ここにパターンを記述
+            var pattern = @"\b(\w)\w(\w)\w\1\b"; //←ここにパターンを記述
             var matches = Regex.Matches(text, pattern);
             foreach (Match m in matches)
                 Console.WriteLine("'{0}'", m.Value);
