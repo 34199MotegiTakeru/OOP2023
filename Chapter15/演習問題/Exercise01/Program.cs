b4523976a5bd2444bb6d8741e7bb028dfc6f5c48
茂木豪琉 <OIBC34199@ojs.ac.jp>
2023-10-17 09:44:49 +0900

    15.1.7と15.1.8

--- Chapter15/演習問題/Exercise01/Program.cs
+++ Chapter15/演習問題/Exercise01/Program.cs
@@ -94,11 +94,33 @@ namespace Exercise01 {
         }
 
         private static void Exercise1_7() {
-
+            var catid = Library.Categories.Single(c => c.Name == "Development").Id;
+            var groups = Library.Books
+                                .Where(b => b.CategoryId == catid)
+                                .GroupBy(b => b.PublishedYear)
+                                .OrderBy(b => b.Key);
+            foreach (var group in groups) {
+                Console.WriteLine("#{0}年",group.Key);
+                foreach (var book in group) {
+                    Console.WriteLine(" {0}", book.Title);
+                }
+            }
         }
 
         private static void Exercise1_8() {
-
+            var query = Library.Categories
+                                .GroupJoin(
+                                    Library.Books,
+                                    c => c.Id,
+                                    b => b.CategoryId,
+                                    (c, b) => new {
+                                        CategoryName = c.Name,
+                                        Count = b.Count()
+                                    })
+                                .Where(x => x.Count >= 4);
+            foreach (var category in query) {
+                Console.WriteLine(category.CategoryName);
+            }
         }
     }
 }
