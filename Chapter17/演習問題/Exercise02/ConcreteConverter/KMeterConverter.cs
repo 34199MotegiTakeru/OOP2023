b254d51e46f3259c08c7c876f8a39fee8f3d64aa
茂木豪琉 <OIBC34199@ojs.ac.jp>
2023-10-19 12:04:13 +0900

    Exercise01、Exercise02　完成

---
+++ Chapter17/演習問題/Exercise02/ConcreteConverter/KMeterConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02.ConcreteConverter {
+    public class KMeterConverter :Framework.ConverterBase{
+        public override bool IsMyUnit(string name) {
+            return name.ToLower() == "km" || name == UnitName;
+        }
+        protected override double Ratio { get { return 1000; } }
+        public override string UnitName { get { return "キロメートル"; } }
+    }
+}
+
